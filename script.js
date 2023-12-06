9c958a4b4c20dc7b3c102aa6ad004a360ee04294
Ari <arisenger0@gmail.com>
2023-12-06 14:15:46 +0100

    fix: remove classList.add

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const btn = document.querySelector("button");
 const bodyDark = document.querySelector("body");
-btn.classList.add("button--dark");
-bodyDark.classList.add("body--dark");
 
 function buttonClick() {
   btn.classList.toggle("button--dark");
