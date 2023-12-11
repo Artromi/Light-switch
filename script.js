24294fb81161ccbea732244171c126e6146c3a56
Ari <arisenger0@gmail.com>
2023-12-11 08:03:28 +0100

    fix: document.title

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const bodyDark = document.querySelector("body");
 function buttonClick() {
   btn.classList.toggle("button--dark");
   bodyDark.classList.toggle("body--dark");
-  // document.title = "Good Night";
-  // document.title = "Good Morning";
+  document.title = "Good Night";
+  if (document.title != "Good Night") {
+    document.title = "Good Night";
+  }
+  
 }
 
 btn.addEventListener("click", buttonClick);
